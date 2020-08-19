30517b55773ca7f6a009d67cfa80f0b9c5f10c5f
SsangWoo <tkddn204@gmail.com>
2020-08-19 15:10:03 +0900

    Updates

---
+++ precache-manifest.90c07df8d01cb72f4555a92e91b54897.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7864e4749b4813c97e9e6f33030bf840",
+    "url": "/clone-youtube/index.html"
+  },
+  {
+    "revision": "c34ea5ba1587d0b6b220",
+    "url": "/clone-youtube/static/js/2.08f46762.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/clone-youtube/static/js/2.08f46762.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8a62e211aadc090fd52",
+    "url": "/clone-youtube/static/js/main.dfdf9ab8.chunk.js"
+  },
+  {
+    "revision": "4f576aabbb49fe30fe3f",
+    "url": "/clone-youtube/static/js/runtime-main.a2df1340.js"
+  }
+]);
